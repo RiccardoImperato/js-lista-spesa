30e154d3862fd72d6c38b52d963bdf74510cb3ce
RiccardoImperato <riccardo.imperato@libero.it>
2023-11-13 14:04:52 +0100

    feat: lista della spesa

---
+++ js/main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// Lista della spesa
+const spesa = ['Pasta', 'Pane', 'Caffè', 'Acqua'];
+console.log(spesa);
+
+
+// Ciclo while
+let i = 0;
+
+while (i < spesa.length) {
+    console.log(spesa[i]);
+    i++;
+}
+
